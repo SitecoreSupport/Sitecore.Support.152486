ef2cc906af0706cbc3dddb4a6c972fa552a98fdc
DK\pk <pk@sitecore.net>
2017-03-17 16:14:02 +0200

    Initial commit

---
+++ src/Sitecore.Support.152486/DataExchange/Providers/Sc/Local/Processors/PipelineSteps/ReadContactsStepProcessor.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.Analytics.Tracking;
+using Sitecore.localhost;
+
+namespace Sitecore.Support.DataExchange.Providers.Sc.Local.Processors.PipelineSteps
+{
+  public class ReadContactsStepProcessor:Sitecore.DataExchange.Providers.Sc.Local.Processors.PipelineSteps.ReadContactsStepProcessor
+  {
+    protected override bool ShouldHandleContact(Contact contact)
+    {
+      return (base.ShouldHandleContact(contact) && !string.IsNullOrEmpty(contact.Identifiers.Identifier));
+    }
+  }
+}
---
+++ src/Sitecore.Support.152486/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.152486")]
+[assembly: AssemblyProduct("Sitecore.Support.152486")]
+[assembly: ComVisible(false)]
